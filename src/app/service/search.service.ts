e8cb3993265166841dc8b32f3c8ed09f367a4794
l3miage-mansosam <mansoursami98@gmail.com>
2025-05-08 18:18:20 +0200

    Premier commit de mon projet

---
+++ src/app/service/search.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IBusScheduleDetails,User,ApiResponse, IBusBooking} from '../model/model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private apiUrl = 'https://api.freeprojectapi.com/api/BusBooking';
+
+  constructor(private http: HttpClient) { }
+
+  searchBus(fromLocationId: string, toLocationId: string, date: string) {
+    return this.http.get(`https://api.freeprojectapi.com/api/BusBooking/searchBus2?fromLocation=${fromLocationId}&toLocation=${toLocationId}&travelDate=${date}`);
+
+  }
+  getBusScheduleById(scheduleId: number): Observable<IBusScheduleDetails> {
+    console.log("scheduleId", scheduleId);
+    return this.http.get<IBusScheduleDetails>(`https://api.freeprojectapi.com/api/BusBooking/GetBusScheduleById?id=${scheduleId}`);
+  }
+  postNewUser(userObj:any){
+    return this.http.post<any>('https://api.freeprojectapi.com/api/BusBooking/AddNewUser', userObj);
+    
+  }
+
+  registerUser(userObj: User): Observable<ApiResponse<null>> {
+    return this.http.post<ApiResponse<null>>(`${this.apiUrl}/AddNewUser`, userObj);
+  }
+
+  loginUser(credentials: { userName: string, password: string }): Observable<ApiResponse<User>> {
+    return this.http.post<ApiResponse<User>>(`${this.apiUrl}/login`, credentials);
+  }
+  createNewBooking(obj:IBusBooking){
+    return this.http.post(`${this.apiUrl}/PostBusBooking`,obj)
+  }
+}
+
